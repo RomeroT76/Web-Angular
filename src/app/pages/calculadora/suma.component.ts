3c2c9c9d537598e1b36dd81006374a743094f6b3
RomeroT76 <robertoart76x@gmail.com>
2024-04-30 23:40:03 -0500

    Calculator functionalities

--- src/app/pages/calculadora/suma.component.ts
+++ src/app/pages/calculadora/suma.component.ts
@@ -8,5 +8,31 @@ import { Component } from '@angular/core';
     imports: []
 })
 export class SumaComponent {
+    public keys: string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+    public oKeys: string[] = ['+', '-', '*', '/']
+    public lsTxt: string = ''
+    public usTxt: string = '0'
 
+    clickEvt(value: string): void {
+        if (this.lsTxt === '' && this.oKeys.includes(value, 0)) {
+            this.lsTxt = this.usTxt + value
+        }
+        else {
+            this.lsTxt = this.lsTxt + value
+        }
+    }
+
+    calculate(expresion: string): void {
+        this.usTxt = eval(expresion);
+        this.lsTxt = ''
+    }
+
+    deleteChar(): void {
+        this.lsTxt = this.lsTxt.substring(0, this.lsTxt.length - 1)
+    }
+
+    clearDisplay(): void {
+        this.lsTxt = ''
+    }
 }
+
